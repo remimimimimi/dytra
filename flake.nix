67bc200a3bb227fb5311e9b648fd01bc2f71caae
remimimimimi <remimimimimi@protonmail.com>
2022-10-01 19:50:17 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+
+{
+  description = "Universal dynamic translator from one architecture to other";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, naersk, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "8len3i8oTwJSOJZMosGGXHBL5BVuGQnWOT2St5YAUFU=";
+        };
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        };
+      in rec {
+        # `nix build`
+        packages = {
+          deltashare = naersk-lib.buildPackage {
+            pname = "dytra";
+            root = ./.;
+          };
+        };
+
+        defaultPackage = packages.deltashare;
+
+        # `nix run`
+        apps.deltashare = flake-utils.lib.mkApp { drv = packages.deltashare; };
+        defaultApp = apps.deltashare;
+
+        # `nix develop`
+        devShell = pkgs.mkShell { nativeBuildInputs = [ rust-toolchain ]; };
+      });
+}
